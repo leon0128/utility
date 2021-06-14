1729afb152f6a165eeb198e58563f39ca8358734
leon0128 <18aj099@ms.dendai.ac.jp>
2021-06-14 11:12:59 +0900

    [update] declares isCallableOperator() function

--- src/output.hpp
+++ src/output.hpp
@@ -3,73 +3,89 @@
 
 #include <ostream>
 #include <tuple>
+#include <type_traits>
 #include <utility>
 
-// for contaner
+// whether OutputT type is callable std::operator(decltype(os), OutputT)
+template<class OutputT
+    , class CharT
+    , class TraitsT>
+auto isCallableOperator(std::basic_ostream<CharT, TraitsT> &os)
+    -> decltype(std::operator <<(os, std::declval<OutputT>())
+        , std::true_type{});
+template<class>
+auto isCallableOperator(...)
+    -> decltype(std::false_type{});
+
+// for container (using iterator)
 template<class CharT
     , class TraitsT
-    , class Container>
-    auto operator <<(std::basic_ostream<CharT, TraitsT> &os
-        , Container &&container)
-        -> decltype(container.begin()
-            , container.end()
-            , os);
+    , class ContainerT>
+auto operator <<(std::basic_ostream<CharT, TraitsT> &os
+    , ContainerT &&container)
+    -> decltype(container.begin()
+        , container.end()
+        , os);
 
-// helper for container
+// helper for contaner
 template<class CharT
     , class TraitsT
     , class IterT>
-    std::basic_ostream<CharT, TraitsT>
-        &containerHelper(std::basic_ostream<CharT, TraitsT> &os
-            , IterT first
-            , IterT last);
+std::basic_ostream<CharT, TraitsT>
+    &containerHelper(std::basic_ostream<CharT, TraitsT> &os
+        , IterT first
+        , IterT last);
 
 // std::pair
 template<class CharT
     , class TraitsT
     , class FirstT
     , class SecondT>
-    std::basic_ostream<CharT, TraitsT>
-        &operator <<(std::basic_ostream<CharT, TraitsT> &os
-            , const std::pair<FirstT, SecondT> &pair);
+std::basic_ostream<CharT, TraitsT>
+    &operator <<(std::basic_ostream<CharT, TraitsT> &os
+        , const std::pair<FirstT, SecondT> &pair);
 
 // std::tuple
 template<class CharT
     , class TraitsT
     , class ...TupleT>
-    std::basic_ostream<CharT, TraitsT>
-        &operator <<(std::basic_ostream<CharT, TraitsT> &os
-            , const std::tuple<TupleT...> &tuple);
+std::basic_ostream<CharT, TraitsT>
+    &operator <<(std::basic_ostream<CharT, TraitsT> &os
+        , const std::tuple<TupleT...> &tuple);
 // std::tuple (helper)
 template<std::size_t Index = 0ull
     , class CharT
     , class TraitsT
     , class ...TupleT>
-    std::basic_ostream<CharT, TraitsT>
-        &tupleHelper(std::basic_ostream<CharT, TraitsT> &os
-            , const std::tuple<TupleT...> &tuple);
+std::basic_ostream<CharT, TraitsT>
+    &tupleHelper(std::basic_ostream<CharT, TraitsT> &os
+        , const std::tuple<TupleT...> &tuple);
 
 template<class CharT
     , class TraitsT
-    , class Container>
-    auto operator <<(std::basic_ostream<CharT, TraitsT> &os
-        , Container &&container)
-        -> decltype(container.begin()
-            , container.end()
-            , os)
+    , class ContainerT>
+auto operator <<(std::basic_ostream<CharT, TraitsT> &os
+    , ContainerT &&container)
+    -> decltype(container.begin()
+        , container.end()
+        , os)
 {
-    return containerHelper(os
-        , container.begin()
-        , container.end());
+    if constexpr(decltype(isCallableOperator<ContainerT>(os))::value)
+        return std::operator <<(os
+            , container);
+    else
+        return containerHelper(os
+            , container.begin()
+            , container.end());
 }
 
 template<class CharT
     , class TraitsT
     , class IterT>
-    std::basic_ostream<CharT, TraitsT>
-        &containerHelper(std::basic_ostream<CharT, TraitsT> &os
-            , IterT first
-            , IterT last)
+std::basic_ostream<CharT, TraitsT>
+    &containerHelper(std::basic_ostream<CharT, TraitsT> &os
+        , IterT first
+        , IterT last)
 {
     os << '{';
 
@@ -92,9 +108,9 @@ template<class CharT
     , class TraitsT
     , class FirstT
     , class SecondT>
-    std::basic_ostream<CharT, TraitsT>
-        &operator <<(std::basic_ostream<CharT, TraitsT> &os
-            , const std::pair<FirstT, SecondT> &pair)
+std::basic_ostream<CharT, TraitsT>
+    &operator <<(std::basic_ostream<CharT, TraitsT> &os
+        , const std::pair<FirstT, SecondT> &pair)
 {
     os << '{'
         << pair.first
@@ -108,9 +124,9 @@ template<class CharT
 template<class CharT
     , class TraitsT
     , class ...TupleT>
-    std::basic_ostream<CharT, TraitsT>
-        &operator <<(std::basic_ostream<CharT, TraitsT> &os
-            , const std::tuple<TupleT...> &tuple)
+std::basic_ostream<CharT, TraitsT>
+    &operator <<(std::basic_ostream<CharT, TraitsT> &os
+        , const std::tuple<TupleT...> &tuple)
 {
     os << '{';
 
@@ -126,9 +142,9 @@ template<std::size_t Index
     , class CharT
     , class TraitsT
     , class ...TupleT>
-    std::basic_ostream<CharT, TraitsT>
-        &tupleHelper(std::basic_ostream<CharT, TraitsT> &os
-            , const std::tuple<TupleT...> &tuple)
+std::basic_ostream<CharT, TraitsT>
+    &tupleHelper(std::basic_ostream<CharT, TraitsT> &os
+        , const std::tuple<TupleT...> &tuple)
 {
     os << std::get<Index>(tuple);
 
